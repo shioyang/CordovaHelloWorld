91b7867367eae3d045ef61eabd4592e4e0c451b7
shioyang <shioyang4321@gmail.com>
2015-11-03 15:23:43 +0900

    Remove un-used codes

--- www/js/services.js
+++ www/js/services.js
@@ -6,29 +6,6 @@ angular.module('services', ['ngStorage'])
 			return $localStorage.$default({
 				persons: []
 			}).persons;
-//			return [
-//				{
-//					name: "aaa AAA",
-//					role: "aaa",
-//					place: "aaa",
-//					country: "aaa",
-//					memo: "aaa is aaa."
-//				},
-//				{
-//					name: "bbb BBB",
-//					role: "bbb",
-//					place: "bbb",
-//					country: "bbb",
-//					memo: "bbb is bbb."
-//				},
-//				{
-//					name: "ccc CCC",
-//					role: "ccc",
-//					place: "ccc",
-//					country: "ccc",
-//					memo: "ccc is ccc."
-//				}
-//			];
 		},
 
 		savePersons: function(persons) {
